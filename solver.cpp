71826153ef5e8bb15d4ecad96805fca37fe3a94e
Samuel Kurtzer <samuelk373@gmail.com>
2020-01-28 10:50:55 +1030

    adding test csv's and base file

---
+++ solver.cpp
@@ -0,0 +1,21 @@
+#include <vector>
+#include <string>
+#include <iostream>
+#include <random>
+/*
+vector<int> bruteForceSudokuGenerator(){
+	// Add random numbers until something works
+	random.rand(1,9)
+// first i need too generate a solved square so i can test out the rest of my program.
+// i can brute force generate a subsquare by having a random mix of 1-9 inside
+/*
+vector<int> subSudoGenerator(){
+	vector<int> ret()
+*/	
+
+int main(){
+	for(int i = 0; i < 100; i++){
+		int n = rand(9);
+		std::cout<<n<<std::endl;
+	}
+}
