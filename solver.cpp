bed92ed573e0382137bef2a609b92b25015e6041
Samuel Kurtzer <samuelk373@gmail.com>
2020-02-03 14:47:18 +1030

    adding small tweeaks and moving to main computer

--- solver.cpp
+++ solver.cpp
@@ -2,20 +2,35 @@
 #include <string>
 #include <iostream>
 #include <random>
-/*
-vector<int> bruteForceSudokuGenerator(){
-	// Add random numbers until something works
-	random.rand(1,9)
-// first i need too generate a solved square so i can test out the rest of my program.
-// i can brute force generate a subsquare by having a random mix of 1-9 inside
-/*
-vector<int> subSudoGenerator(){
-	vector<int> ret()
-*/	
+#include <set>
+#include <fstream>
+
+typedef vector<vector<int>> grid;
+
+vector<int> listAllRelatedSquares(grid puzzle, int x, int y){
+	/* List all values that are releted to the given square*/
+	set<int> usedNumbers;
+	for(int i=0; i<9; i++){
+		usedNumbers.insert(puzzle[x][i])
+		usedNumbers.insert(puzzle[i][y])
+	}
+}
+
+bool importSudokus(std::string path, grid puzzle, grid solution){
+	/* Function for importing sudokus from a file.
+		uses 81 char lines of either . or [1..9] to denote
+		a Puzzle followed by an 81 char solution.
+	*/
+	ifstream puzzleList;
+	
+}
+
+vector<int> listPossibleValues(grid puzzle, int dist){
+	// Return a list of all possible values for a square.
+}
+
+
 
 int main(){
-	for(int i = 0; i < 100; i++){
-		int n = rand(9);
-		std::cout<<n<<std::endl;
-	}
+
 }
